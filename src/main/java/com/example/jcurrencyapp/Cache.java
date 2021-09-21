4d502d662e69e828e30f16c0badedcefa88b7580
Arkita Emil <emil.arkita@streamsoft.pl>
2021-09-21 14:01:19 +0200

    Update logic to multiple providers

---
+++ src/main/java/com/example/jcurrencyapp/Cache.java
@@ -0,0 +1,30 @@
+package com.example.jcurrencyapp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.jcurrencyapp.model.CurrencyTypes;
+
+public class Cache {
+
+	private Map<String, BigDecimal> cache = new HashMap<String, BigDecimal>();
+	
+	public void putRate(CurrencyTypes code, LocalDate date, BigDecimal rate) {
+		cache.put(createKey(code, date), rate);
+	}
+	
+	public BigDecimal getRate(CurrencyTypes code, LocalDate date) {
+		return cache.get(createKey(code, date));
+	}
+	
+	public void clear() {
+		this.cache.clear();
+	}
+	
+	private String createKey(CurrencyTypes code, LocalDate date) {
+		return code.toString() + date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+	}
+}
--- src/test/java/com/example/jcurrencyapp/data/provider/impl/NbpJsonProviderTest.java
+++ src/test/java/com/example/jcurrencyapp/data/provider/impl/NbpJsonProviderTest.java
@@ -1,10 +1,14 @@
 package com.example.jcurrencyapp.data.provider.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDate;
-import static org.assertj.core.api.Assertions.*;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.example.jcurrencyapp.data.converter.impl.NbpJsonConverter;
+import com.example.jcurrencyapp.model.CurrencyTypes;
 
 public class NbpJsonProviderTest {
 
@@ -12,16 +16,16 @@ public class NbpJsonProviderTest {
 	public void init() {
 		System.out.println("Testing: " + this.getClass().getName());
 	}
-	
+
 	@Test
 	public void shouldGiveValidResponse_WhenGivenNbpJsonProvider() {
-		//Given
-		NbpJsonProvider provider = new NbpJsonProvider();
+		// Given
+		NbpJsonProvider provider = new NbpJsonProvider(new NbpJsonConverter());
 		String validResponse = "{\"table\":\"C\",\"currency\":\"dolar amerykański\",\"code\":\"USD\",\"rates\":[{\"no\":\"070/C/NBP/2016\",\"effectiveDate\":\"2016-04-12\",\"bid\":3.6949,\"ask\":3.7695}]}";
 
 		// When
-		String response = provider.getData("USD", LocalDate.of(2016, 4, 12));
-		
+		String response = provider.getData(CurrencyTypes.USD, LocalDate.of(2016, 4, 12));
+
 		// Then
 		assertThat(response).isEqualTo(validResponse);
 	}
